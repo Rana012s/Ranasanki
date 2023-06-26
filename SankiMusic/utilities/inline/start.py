d9f22df1b6438babcec5faff05d19183b8498c17
Rana012s <128143630+Rana012s@users.noreply.github.com>
2023-06-26 22:18:35 +0530

    Add files via upload

---
+++ SankiMusic/utilities/inline/start.py
@@ -0,0 +1,51 @@
+from typing import Union
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from SankiMusic.utilities.config import config
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="≽ 𝐂ᴏᴍᴍᴀɴᴅs ≼",
+                url=f"https://t.me/{BOT_USERNAME}?start=help",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✮ 𝐒ᴇᴛᴛɪɴɢs ✮", callback_data="settings_helper"
+            )
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="𝐀ᴅᴅ 𝐌ᴇ 𝐓ᴏ 𝐘ᴏᴜʀ 𝐆ʀᴏᴜᴘ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❀⋟ 𝐇ᴇʟᴘ ⋞❀", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✭ 𝐔ᴘᴅᴀᴛᴇs ✭", url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text="✭ 𝐒ᴜᴘᴘᴏʀᴛ ✭", url=config.SUPPORT_GROUP
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✮ 𝐒ᴏᴜʀᴄᴇ ✮", url="https://te.legra.ph/file/5fa7b4d86dcd5720ef30c.mp4"
+            )
+        ]
+     ]
+    return buttons
